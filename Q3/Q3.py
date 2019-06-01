26b819c46e639c9414d21b9fec9453dacfbbdfb6
masumesajadi <42239171+masumesajadi@users.noreply.github.com>
2019-06-01 05:38:49 +0430

    Update and rename Q3.py to Q3/Q3.py

--- Q3/Q3.py
+++ Q3/Q3.py
@@ -7,8 +7,9 @@ Created on Fri May 31 00:23:59 2019
 
 A0=dict(zip(('a','b','c','d','e'),('1','2','3','4','5')))
 A1=range(10)
-for n in A1:
-    print(n)
 A2=[i for i in A1 if i in A0]
 A3=sorted(A0[i] for i in A0)
 A4=[[i,i*i]for i in A1]
+
+for i in A0,A1,A2,A3,A4:
+    print(i)
